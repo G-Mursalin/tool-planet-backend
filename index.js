ec25c80dc1875e3314b2dfd5a3758cbe0aaecefb
Golam Mursalin <golammursalin309@gmail.com>
2022-05-24 00:34:44 +0600

    JWT token verification function added

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Verifying Token From User
+const verifyJWT = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorize Access" });
+  }
+
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 // Database Connection
 const uri = `mongodb+srv://${process.env.dbUser}:${process.env.dbPassword}@cluster0.njw5u.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -62,7 +79,7 @@ async function run() {
       });
       res.send(result);
     });
-    // Get all orders
+    // Get all orders (orderForm component)
     app.post("/orders", async (req, res) => {
       const exists = await orderCollection.findOne({
         productId: req.body.productId,
